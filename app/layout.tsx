354f670ebcaf90eb07e5c92fba2a2dbb99cadd82
A3939 <adityagpatel59@gmail.com>
2025-01-05 20:30:29 +0530

    feat: file upload, share and all feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Poppins } from 'next/font/google'
+
 import "./globals.css";
-import {pop} from "@jridgewell/set-array";
 
 const poppins = Poppins({
-    variable: "--font-poppins",
-    subsets: ["latin"],
-    weight: ["100","200","300","400","500","600","700","800","900"],
-});
-
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-poppins',
+})
 
 export const metadata: Metadata = {
   title: "StoreIt",
-  description: "StoreIt - the one storage solution.",
+  description: "StoreIt - The only storage solution you need.",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex-center">
-      <h1 className="h1">StoreIt - The only storage solutions you need.</h1>
-    </div>
-  );
-}
